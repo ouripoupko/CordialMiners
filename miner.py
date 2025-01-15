93c94701029976296db88daa6edb7e8ffb75e797
ouripoupko <ouri.poupko@gmail.com>
2025-01-15 11:07:41 +0200

    runs until last_final_leader

--- miner.py
+++ miner.py
@@ -28,7 +28,7 @@ class Miner:
         f = (n - 1) // 3
         self.super_majority = (n + f) // 2
         self.me = me
-        self.round = 0
+        self.round = -1
         self.round_collection = {}
         self.next_non_final_round = 0
         self.max_depth = 0
@@ -61,8 +61,8 @@ class Miner:
                  CREATOR: self.me,
                  TIMESTAMP: datetime.now().strftime('%Y%m%d%H%M%S%f'),
                  # TODO: check if can use tips directly
-                 POINTERS: [key for key in self.blocklace if self.blocklace[key][DEPTH] == self.round],
-                 DEPTH: self.round + 1}
+                 POINTERS: [key for key in self.blocklace if self.blocklace[key][DEPTH] == self.round-1],
+                 DEPTH: self.round}
         block['hash_code'] = hashlib.sha256(str(block).encode('utf-8')).hexdigest()
         return block
 
@@ -85,10 +85,10 @@ class Miner:
         return reply
 
     # TODO: make sure we don't use this horrible function
-    def equivocation(self, b1, b2):
-        c1 = self.blocklace[b1][CREATOR] == self.blocklace[b2][CREATOR]
-        c2 = self.observes(b1, b2)
-        c3 = self.observes(b2, b1)
+    def equivocation(self, key1, key2):
+        c1 = self.blocklace[key1][CREATOR] == self.blocklace[key2][CREATOR]
+        c2 = self.observes(key1, key2)
+        c3 = self.observes(key2, key1)
         return c1 and not c2 and not c3
 
     def equivocator(self, q, b):
@@ -103,12 +103,13 @@ class Miner:
         e = c and d and block[DEPTH] == 0 and len(block[POINTERS]) == 0
         f = d and block[DEPTH] > 0
         g = c and f and len(block[POINTERS]) > self.super_majority
-        return a and b and c and d and (e or g)
+        reply = a and b and c and d and (e or g)
+        return reply
 
     # TODO: check - should a block approve itself?
-    def approves(self, head, block):
+    def approves(self, head, key):
         children = set(self.blocklace[head][POINTERS])
-        creator = self.blocklace[block][CREATOR]
+        creator = self.blocklace[key][CREATOR]
         # instead of checking every block in the closure of 'head', if it equivocates 'block'
         # check all tips of the equivocator. if a tip observes 'block' it is safe
         # if it doesn't, enough to check if the leaf of that tip is in the closure of 'head'
@@ -116,36 +117,36 @@ class Miner:
         if creator in self.equivocators:
             equivocating_blocks = {self.leaf_of_creator(tip)
                                    for tip in self.equivocators[creator]
-                                   if not self.observes(tip, block)}
+                                   if not self.observes(tip, key)}
         in_tree = False
         equivocate = False
         while children and not equivocate:
             equivocate = not children.isdisjoint(equivocating_blocks)
-            if block in children:
+            if key in children:
                 in_tree = True
             children = {grand_child for child in children for grand_child in self.blocklace[child][POINTERS]}
         return in_tree and not equivocate
 
-    def ratifies(self, head, block):
-        depth = self.blocklace[block][DEPTH]
+    def ratifies(self, head, key):
+        depth = self.blocklace[key][DEPTH]
         approvers = set()
         observers = {head}
         while observers:
             observer = observers.pop()
-            if self.approves(observer, block):
+            if self.approves(observer, key):
                 approvers.add(self.blocklace[observer][CREATOR])
             observers.update({child
                               for child in self.blocklace[observer][POINTERS]
                               if self.blocklace[child][DEPTH] >= depth})
         return len(approvers) > self.super_majority
 
-    def super_ratifies(self, head, block):
-        depth = self.blocklace[block][DEPTH]
+    def super_ratifies(self, head, key):
+        depth = self.blocklace[key][DEPTH]
         ratifiers = set()
         observers = {head}
         while observers:
             observer = observers.pop()
-            if self.ratifies(observer, block):
+            if self.ratifies(observer, key):
                 ratifiers.add(self.blocklace[observer][CREATOR])
                 observers.update({child
                                   for child in self.blocklace[observer][POINTERS]
@@ -157,20 +158,21 @@ class Miner:
 
     def cordial_round(self, cycle):
         creators = {block[CREATOR] for block in self.blocklace.values() if block[DEPTH] == cycle}
+        logger.debug(f'round {cycle} has {len(creators)} creators')
         return len(creators) > self.super_majority
 
     # Algorithm 2
-    def tau(self, block):
-        final_leader = self.last_final_leader(block)
+    def tau(self, key):
+        final_leader = self.last_final_leader(key)
         if final_leader:
             self.tau_prime(final_leader)
 
-    def tau_prime(self, block):
-        if block is None or block in self.outputBlocks:
+    def tau_prime(self, key):
+        if key is None or key in self.outputBlocks:
             return
-        previous = self.previous_ratified_leader(block)
+        previous = self.previous_ratified_leader(key)
         self.tau_prime(previous)
-        output = self.xsort(block, self.closure(block) - self.closure(previous))
+        output = self.xsort(key, self.closure(key) - self.closure(previous))
         logger.debug(output)
         self.outputBlocks.update(output)
 
@@ -178,10 +180,10 @@ class Miner:
         # TODO: unclear how to sort
         return list(candidates, key=lambda x: self.blocklace[x][DEPTH])
 
-    def arg_max_depth(self, blocks):
+    def arg_max_depth(self, keys):
         max_depth = 0
         reply = None
-        for key in blocks:
+        for key in keys:
             if self.blocklace[key][DEPTH] > max_depth:
                 max_depth = self.blocklace[key][DEPTH]
                 reply = self.blocklace[key]
@@ -195,8 +197,9 @@ class Miner:
              and self.blocklace[block][CREATOR] == self.leader(self.blocklace[block][DEPTH])}
         return self.arg_max_depth(R)
 
-    def last_final_leader(self, block):
-        depth = block(DEPTH)
+    def last_final_leader(self, key):
+        block = self.blocklace[key]
+        depth = block[DEPTH]
         previous_final = self.blocklace.get(self.previous_final_leader)
         if previous_final and depth <= previous_final[DEPTH]:
             return None
@@ -211,12 +214,12 @@ class Miner:
         leader_key = collection['leader']
         leader_block = self.blocklace[leader_key]
         if block[CREATOR] not in collection['ratifications']:
-            if self.approves(block, leader_block):
+            if self.approves(key, leader_key):
                 collection['ratifications'][block[CREATOR]] = block[HASHCODE]
         if len(collection['ratifications']) <= self.super_majority:
             return None
         if block[CREATOR] not in collection['super']:
-            if self.ratifies(block, {self.blocklace[key] for key in collection['ratifications'].values()}):
+            if self.ratifies(key, {self.blocklace[key2] for key2 in collection['ratifications'].values()}):
                 collection['super'][block[CREATOR]] = block[HASHCODE]
         if len(collection['super']) <= self.super_majority:
             return None
@@ -229,33 +232,37 @@ class Miner:
         completed = self.completed_round()
         logger.debug(f'completed round {completed} and I am in round {self.round}')
         if completed >= self.round:
+            self.round = completed + 1
             block = self.create_block(self.messages)
             logger.debug(f'create block {block}')
             self.messages = []
-            self.round = block[DEPTH]
-            logger.debug(f'send it to {self.others}')
             for agent in self.others:
                 requests.post(f'http://localhost:{agent}/blocks', json=[block])
             self.buffer[block[HASHCODE]] = block
             while self.process_buffer():
                 continue
+            logger.debug(f'\nbuffer: {list(self.buffer.keys())}\naccepted: {list(self.blocklace.keys())}')
 
     def receive_block(self, block):
         if self.correct_block(block):
             self.buffer[block[HASHCODE]] = block
         while self.process_buffer():
             continue
+        logger.debug(f'\ncreator {block[CREATOR]}\ndepth {block[DEPTH]}\nbuffer: {list(self.buffer.keys())}\naccepted: {list(self.blocklace.keys())}')
 
     def process_buffer(self):
         should_repeat = False
+        to_delete = []
         for key in self.buffer:
             block = self.buffer[key]
             dangling_pointers = [pointer for pointer in block[POINTERS] if pointer not in self.blocklace]
             if not dangling_pointers and self.cordial_block(block):
                 self.accept_block(block)
-                del self.buffer[key]
-                self.tau(block)
+                to_delete.append(key)
+                self.tau(key)
                 should_repeat = True
+        for key in to_delete:
+            del self.buffer[key]
         return should_repeat and self.buffer
 
     def accept_block(self, block):
@@ -310,7 +317,7 @@ class Miner:
         return len(creators) > self.super_majority
 
     def async_completed_round(self):
-        cycle = 1
+        cycle = 0
         while self.cordial_round(cycle):
             cycle += 1
         return cycle-1
@@ -318,7 +325,7 @@ class Miner:
     def es_leader(self, depth):
         if depth % self.wavelength:
             return None
-        return self.everyone[(depth / self.wavelength) % len(self.everyone)]
+        return self.everyone[(depth // self.wavelength) % len(self.everyone)]
 
     def es_final_leader(self, block):
         depth = block[DEPTH] + self.wavelength
@@ -334,7 +341,7 @@ class Miner:
 
     # TODO: copying from async. We defer from the paper here
     def es_completed_round(self):
-        cycle = 1
+        cycle = 0
         while self.cordial_round(cycle):
             cycle += 1
         return cycle-1
